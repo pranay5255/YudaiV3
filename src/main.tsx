7277fdb628d0432f4c5f87a7b0efb92bbb188c82
pranay5255 <pranay5255@yahoo.com>
2025-08-06 19:45:50 +0530

    fix: removed Session provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthProvider';
 import { RepositoryProvider } from './contexts/RepositoryProvider';
-import { SessionProvider } from './contexts/SessionContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <SessionProvider>
         <RepositoryProvider>
           <App />
         </RepositoryProvider>
-      </SessionProvider>
     </AuthProvider>
   </StrictMode>
 );
