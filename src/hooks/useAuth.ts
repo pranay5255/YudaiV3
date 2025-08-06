b69b74430b2e4f7ae434fef40a51c9bcb2596e58
pranay5255 <pranay5255@yahoo.com>
2025-08-06 20:23:02 +0530

    fix: studpid import error fix

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,1 +1,10 @@
-export { useAuth } from '../contexts/AuthProvider'; 
+import { useContext } from "react";
+import { AuthContext, AuthContextValue } from "../contexts/AuthProvider";
+
+export const useAuth = (): AuthContextValue => {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+      throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+  };
