0bfaca7f634d83905af4b01b8bacfb2055916e6b
pranay5255 <pranay5255@yahoo.com>
2026-03-05 16:49:01 +0000

    Update src/remotion/Root.tsx

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,9 +1,26 @@
 import { Composition, Folder } from 'remotion';
 import { YudaiLoginDemo, type YudaiLoginDemoProps } from './YudaiLoginDemo';
+import {
+  YudaiFounderTwitterIntro,
+  type YudaiFounderTwitterIntroProps,
+} from './YudaiFounderTwitterIntro';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Folder name="Product-Demos">
+      <Composition
+        id="YudaiFounderTwitterIntro18s"
+        component={YudaiFounderTwitterIntro}
+        durationInFrames={540}
+        fps={30}
+        width={1080}
+        height={1350}
+        defaultProps={
+          {
+            productName: 'YudaiV3',
+          } satisfies YudaiFounderTwitterIntroProps
+        }
+      />
       <Composition
         id="YudaiLoginDemo15s"
         component={YudaiLoginDemo}
