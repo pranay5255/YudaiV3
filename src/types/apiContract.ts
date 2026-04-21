e4a8e74e3a28cbfbf77da999939d78173d0b5da4
pranay5255 <pranay5255@yahoo.com>
2026-04-21 15:32:12 +0000

    refactor: expose api contract type aliases

--- src/types/apiContract.ts
+++ src/types/apiContract.ts
@@ -4,21 +4,18 @@ export type ApiPaths = paths;
 export type ApiSchemas = components['schemas'];
 export type ApiSchema<Name extends keyof ApiSchemas> = ApiSchemas[Name];
 
-export namespace ApiContract {
-  export type Session = ApiSchema<'SessionResponse'>;
-  export type SessionContext = ApiSchema<'SessionContextResponse'>;
-  export type ChatMessage = ApiSchema<'ChatMessageResponse'>;
-  export type ChatRequest = ApiSchema<'ChatRequest'>;
-  export type ChatResponse = ApiSchema<'ChatResponse'>;
-  export type CreateSessionRequest = ApiSchema<'CreateSessionRequest'>;
-  export type UpdateSessionRequest = ApiSchema<'UpdateSessionRequest'>;
-  export type ExecutionRequest = ApiSchema<'ExecutionRequest'>;
-  export type ExecutionResponse = ApiSchema<'ExecutionResponse'>;
-  export type ExecutionStatus = ApiSchema<'ExecutionStatusResponse'>;
-  export type Runtime = ApiSchema<'RuntimeResponse'>;
-  export type Sandbox = ApiSchema<'SandboxResponse'>;
-  export type GitHubRepository = ApiSchema<'GitHubRepositoryResponse'>;
-  export type GitHubBranch = ApiSchema<'GitHubBranchResponse'>;
-  export type User = ApiSchema<'ValidateSessionResponse'>;
-}
-
+export type Session = ApiSchema<'SessionResponse'>;
+export type SessionContext = ApiSchema<'SessionContextResponse'>;
+export type ChatMessage = ApiSchema<'ChatMessageResponse'>;
+export type ChatRequest = ApiSchema<'ChatRequest'>;
+export type ChatResponse = ApiSchema<'ChatResponse'>;
+export type CreateSessionRequest = ApiSchema<'CreateSessionRequest'>;
+export type UpdateSessionRequest = ApiSchema<'UpdateSessionRequest'>;
+export type ExecutionRequest = ApiSchema<'ExecutionRequest'>;
+export type ExecutionResponse = ApiSchema<'ExecutionResponse'>;
+export type ExecutionStatus = ApiSchema<'ExecutionStatusResponse'>;
+export type Runtime = ApiSchema<'RuntimeResponse'>;
+export type Sandbox = ApiSchema<'SandboxResponse'>;
+export type GitHubRepository = ApiSchema<'GitHubRepositoryResponse'>;
+export type GitHubBranch = ApiSchema<'GitHubBranchResponse'>;
+export type User = ApiSchema<'ValidateSessionResponse'>;
